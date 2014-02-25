b804f6023b04967a8a94078358d608983af8e8a5
Manny <mjl288@scarletmail.rutgers.edu>
2014-02-25 00:25:44 -0500

    started connect method

--- src/RUBTclient/Peer.java
+++ src/RUBTclient/Peer.java
@@ -1,12 +1,13 @@
 package RUBTclient;
 
+import java.net.*;
+import java.io.*;
 
 public class Peer {
 	
 	String ip;
 	String peer_id;
 	Integer port;
-	
 	
 	public Peer(String ip, String peer_id, Integer port) {
 		super();
@@ -15,4 +16,20 @@ public class Peer {
 		this.port = port;
 	}
 		
+	public void connectToPeer(Message handshake) throws IOException{
+		
+		Socket peerConnection = null;
+		OutputStream peerOutputStream = null;
+		InputStream peerInputStream = null;
+		
+		try{
+			peerConnection = new Socket(ip, port);
+			peerOutputStream = peerConnection.getOutputStream();
+			peerInputStream = peerConnection.getInputStream();
+		}catch(UnknownHostException e){
+			System.out.println("UnknownHostException");
+		}catch(IOException e){
+			System.out.println("IOException");
+		}
+	}
 }
