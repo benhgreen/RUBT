e5eb9e7f76dab0c50c25eca0269285fc50dc63aa
chris-rios <rioschm@gmail.com>
2014-02-26 22:41:09 -0500

    Added Getters, and method that generates request message

--- src/RUBTclient/Message.java
+++ src/RUBTclient/Message.java
@@ -1,14 +1,15 @@
 package RUBTclient;
 
+import java.nio.ByteBuffer;
 import java.util.Arrays;
 
 public class Message 
 
 {
-	 private byte[] handshake = new byte[68];
 	 private final byte[] handshake_consts = {0x13,'B','i','t','T','o','r','r','e','n','t',' ','p','r','o','t','o','c','o','l',0,0,0,0,0,0,0,0};
-	 private final byte[] have_conts = {0,0,0,5,4};
-	 //all non-payload messages
+	 private final byte[] have_consts = {0,0,0,5,4};
+	 private final byte[] request_consts = {0,0,1,3,6};
+	//all non-payload messages
 	 private final byte[] choke = { 0,0,0,1,0};
 	 private final byte[] unchoke = {0,0,0,1,1};
 	 private final byte[] interested = {0,0,0,1,2};
@@ -26,6 +27,7 @@ public class Message
 	 */
 	public byte[] handShake( byte[] info_hash)
 	{
+		byte[] handshake = new byte[68];
 		byte[] userid;
 		System.arraycopy(handshake_consts,0,handshake,0,28);// copies handshake constants
 		System.arraycopy(info_hash, 0, handshake,28 , 20);
@@ -33,6 +35,45 @@ public class Message
 		userid=our_id.getUser_id().getBytes();    //gets our user id, and then puts it into our handshake
 		System.arraycopy(userid, 0, handshake,48 , 20);
 		return handshake;
+	}
+	
+	/**
+	 * This method takes all inputs and constants for a request message
+	 * and generates returns a complete request message.
+	 * @param index index of the piece wanted
+	 * @param begin offset of the byte inside of the piece
+	 * @param length length that we want to download
+	 * @return returns a composed request byte[] message
+	 */
+	public byte[] request(int index, int begin, int length)
+	{
+		ByteBuffer request = ByteBuffer.allocate(20);//need a better way, maybe bytearray output stream?
+		request.put(request_consts);
+		request.putInt(index);
+		request.putInt(begin);
+		request.putInt(length);
+		return request.array();
+	}
+	
+	public byte[] getChoke() 
+	{
+		return choke;
+	}
+	public byte[] getUnchoke() 
+	{
+		return unchoke;
+	}
+	public byte[] getInterested() 
+	{
+		return interested;
+	}
+	public byte[] getNot_interested() 
+	{
+		return not_interested;
+	}
+	public byte[] getKeep_alive() 
+	{
+		return keep_alive;
 	}
 }
 
