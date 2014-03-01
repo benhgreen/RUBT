a74115ff8d7ab0449f840368a316c1c397bb596b
Ben Green <ben.green@inventati.org>
2014-03-01 00:24:09 -0500

    get fucked chris

--- src/RUBTclient/Piece.java
+++ src/RUBTclient/Piece.java
@@ -8,40 +8,12 @@ public class Piece {
 	/**
 	 * @return the data
 	 */
-	public byte[] getData() {
-		return data;
-	}
-	/**
-	 * @param data the data to set
-	 */
-	public void setData(byte[] data) {
+	
+	public Piece(byte[] data, int piece, int offset){
 		this.data = data;
-	}
-	/**
-	 * @return the piece
-	 */
-	public int getPiece() {
-		return piece;
-	}
-	/**
-	 * @param piece the piece to set
-	 */
-	public void setPiece(int piece) {
 		this.piece = piece;
-	}
-	/**
-	 * @return the offset
-	 */
-	public int getOffset() {
-		return offset;
-	}
-	/**
-	 * @param offset the offset to set
-	 */
-	public void setOffset(int offset) {
 		this.offset = offset;
 	}
-	
 	
 
 }
