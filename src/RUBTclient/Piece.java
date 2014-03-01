57c4e832e09f061e579b11ed00be87ea88211b53
chris-rios <rioschm@gmail.com>
2014-03-01 00:37:17 -0500

    added a constructor for piece

--- src/RUBTclient/Piece.java
+++ src/RUBTclient/Piece.java
@@ -5,41 +5,26 @@ public class Piece {
 	byte[] data;
 	int piece;
 	int offset;
-	/**
-	 * @return the data
-	 */
-	public byte[] getData() {
-		return data;
-	}
-	/**
-	 * @param data the data to set
-	 */
-	public void setData(byte[] data) {
+	
+	public Piece(byte[] data, int piece, int offset)
+	{
 		this.data = data;
+		this.piece = piece;
+		this.offset = offset;
 	}
+	
 	/**
 	 * @return the piece
 	 */
 	public int getPiece() {
 		return piece;
 	}
-	/**
-	 * @param piece the piece to set
-	 */
-	public void setPiece(int piece) {
-		this.piece = piece;
-	}
+	
 	/**
 	 * @return the offset
 	 */
 	public int getOffset() {
 		return offset;
-	}
-	/**
-	 * @param offset the offset to set
-	 */
-	public void setOffset(int offset) {
-		this.offset = offset;
 	}
 	
 	
