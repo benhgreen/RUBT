7c2f40f51e4ed72a8a15c39fcca225df0abb7341
Ben Green <ben.green@inventati.org>
2014-03-02 02:48:05 -0500

    Cleaned up code a bit and added some documentation.

--- src/RUBTclient/DestFile.java
+++ src/RUBTclient/DestFile.java
@@ -19,58 +19,55 @@ public class DestFile {
 		this.name = name;
 		this.torrentinfo = torrentinfo;
 		try {
+			//initialize RandomAccessFile and set length
 			this.dest = new RandomAccessFile(name,"rw");
-			//dest.setLength(torrentinfo.file_length);
-			dest.setLength(100);
+			dest.setLength(torrentinfo.file_length);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
-	public void testWrite(String input, Integer offset){
-		try {
-			dest.seek(offset);
-			dest.write(input.getBytes());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
+	/**Takes in a Piece object and writes its data to the location specified by the piece length and offset.
+	 * @param Piece object containing data to add to the target file 
+	 */
 	public void addPiece(Piece piece){
 		try {
+			//calculate location to write data in the file using piece length and offset if applicable
 			long target = piece.piece*torrentinfo.piece_length + piece.offset;
 			dest.seek(target);
 			dest.write(piece.data);
-			System.out.println("added part of piece " + piece.piece + " to pos " + target);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
+	/**
+	 * Closes RandomAccessFile associated with this DestFile.
+	 */
 	public void close(){
 		try {
 			this.dest.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
+	/**
+	 * @param Piece with data to verify
+	 * @return True if the piece's data hash was located in the TorrentInfo's data hash array, false if it was not found.
+	 */
 	public boolean verify(Piece piece){
 		
+		//get data hash
 		MessageDigest md = null;
 		try {
 			md = MessageDigest.getInstance("SHA-1");
 		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 		byte[] hash = md.digest(piece.data);
 		
+		//iterate through torrentinfo piece hashes and look for a match
 		for(int i = 0; i<this.torrentinfo.piece_hashes.length; i++){
 			if(Arrays.equals(hash, this.torrentinfo.piece_hashes[i].array())){
 				return true;
